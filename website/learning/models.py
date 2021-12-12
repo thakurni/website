06ee0a466640c85564a83b94faa53e986b017f18
Nikhil Thakur <thakurnikhilsingh1@gmail.com>
2021-12-12 23:15:32 +0530

    sixth commit

--- website/learning/models.py
+++ website/learning/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.db.models.deletion import CASCADE
 
 # Create your models here.
+class Topic(models.Model):
+    topicName=models.CharField(max_length=100)
+    creationdate=models.DateField(auto_created=True)
+
+
+
+
+class SubTopics(models.Model):
+    topicName=models.ForeignKey(Topic,on_delete=CASCADE)
+    subTopicName=models.CharField(max_length=100)
+    content=models.TextField()
+
+
+
--- website/learning/views.py
+++ website/learning/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse,request
 
+
+
 # Create your views here.
 def home(request):
     return render(request,'index.html')
+
+def topics(request):
+    data=['math','Datascience','web development']
+    return render(request,'topics.html',{'topics':data})
